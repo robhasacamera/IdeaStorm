8a24a1a1aa1916a9dc111d06bdfa6c11a74b860c
Robert Cole <Robert@Roberts-MacBook-Air.local>
2011-10-30 01:50:52 -0500

    Added PenDrawingTool class.

---
+++ IdeaStorm/Drawing Engine/Toolbar/Tool Set/Drawing Tools/PenDrawingTool.h
@@ -0,0 +1,15 @@
+//
+//  PenDrawingTool.h
+//  IdeaStorm
+//
+//  Created by Robert Cole on 10/30/11.
+//  Copyright (c) 2011 Robert Cole. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DrawingTool.h"
+#import "ToolbarItem.h"
+
+@interface PenDrawingTool : ToolbarItem <DrawingTool>
+
+@end
---
+++ IdeaStorm/Drawing Engine/Toolbar/Tool Set/Drawing Tools/PenDrawingTool.m
@@ -0,0 +1,20 @@
+//
+//  PenDrawingTool.m
+//  IdeaStorm
+//
+//  Created by Robert Cole on 10/30/11.
+//  Copyright (c) 2011 Robert Cole. All rights reserved.
+//
+
+#import "PenDrawingTool.h"
+
+@implementation PenDrawingTool
+
+//TODO: Finish stub
+- (Vertex *)verticesFromPoint:(CGPoint)point andDrawingColor:(Color)color andPointSize:(CGFloat)size isLastPoint:(_Bool)lastPoint {
+    Vertex * vertices;
+    
+    return vertices;
+}
+
+@end
