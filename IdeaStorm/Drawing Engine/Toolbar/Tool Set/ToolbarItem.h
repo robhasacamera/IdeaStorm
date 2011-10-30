086dd24eae1e4492d1e395b498998923f50c2ea1
Robert Cole <Robert@Roberts-MacBook-Air.local>
2011-10-29 23:57:28 -0500

    Added class ToolbarItem

---
+++ IdeaStorm/Drawing Engine/Toolbar/Tool Set/ToolbarItem.h
@@ -0,0 +1,20 @@
+//
+//  ToolbarItem.h
+//  IdeaStorm
+//
+//  Created by Robert Cole on 10/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ToolbarItem : NSObject
+
+@property (strong, nonatomic) UIImage *icon;
+@property (strong, nonatomic) NSString *title;
+
+- (id)initWithImageName:(NSString *)imageFilename andTitle:(NSString *)title;
+
+- (bool)setIconWithImageName:(NSString *)imageFilename;
+
+@end
---
+++ IdeaStorm/Drawing Engine/Toolbar/Tool Set/ToolbarItem.m
@@ -0,0 +1,34 @@
+//
+//  ToolbarItem.m
+//  IdeaStorm
+//
+//  Created by Robert Cole on 10/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+
+#import "ToolbarItem.h"
+
+@implementation ToolbarItem
+
+@synthesize icon = _icon;
+@synthesize title = _title;
+
+- (id)initWithImageName:(NSString *)imageFilename andTitle:(NSString *)title {
+    self = [super init];
+    
+    if (self) {
+        self.title = title;
+        
+        [self setIconWithImageName:imageFilename];
+    }
+    
+    return self;
+}
+
+//TODO: Need to finish this stub method
+- (bool)setIconWithImageName:(NSString *)imageFilename {
+    bool imageLoaded = NO;
+    
+    return imageLoaded;
+}
+
+@end
