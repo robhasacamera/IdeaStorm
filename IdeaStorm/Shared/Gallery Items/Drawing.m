b31b942e877c13736602b69b58ca97fa89fac319
Robert Cole <robhasacamera@gmail.com>
2012-03-11 21:23:36 -0500

    Created stub methods for gallery items.

--- IdeaStorm/Shared/Gallery Items/Drawing.m
+++ IdeaStorm/Shared/Gallery Items/Drawing.m
@@ -10,4 +10,54 @@
 
 @implementation Drawing
 
+@synthesize parent = _parent;
+@synthesize children = _children;
+@synthesize pathID = _pathID;
+@synthesize thumbnailImage = _thumbnailImage;
+@synthesize fullImage = _fullImage;
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super init];
+    
+    if (self) {
+        //initialize with data from coder
+    }
+    
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    
+}
+
+- (NSString *)getFullPath {
+    
+    return nil;
+}
+
+- (bool)addChild:(NSObject <GalleryItem> *)galleryItem {
+    
+    return nil;
+}
+
+- (bool)deleteChild:(NSObject <GalleryItem> *)galleryItem {
+    
+    return nil;
+}
+
+- (NSString *)saveThumbnailImage {
+    
+    return nil;
+}
+
+- (NSString *)saveFullImage {
+    
+    return nil;
+}
+
+- (bool)exportToCameraRoll {
+    
+    return nil;
+}
+
 @end
--- IdeaStorm/Shared/Gallery Items/GalleryItem.h
+++ IdeaStorm/Shared/Gallery Items/GalleryItem.h
@@ -10,4 +10,24 @@
 
 @protocol GalleryItem <NSCoding>
 
+@required
+
+@property (nonatomic, strong) NSObject <GalleryItem> *parent;
+@property (nonatomic, strong, readonly) NSMutableArray *children;
+@property (nonatomic, strong, readonly) NSString *pathID;
+@property (nonatomic, strong) UIImage *thumbnailImage;
+@property (nonatomic, strong) UIImage *fullImage;
+
+- (NSString *)getFullPath;
+
+- (bool)addChild:(NSObject <GalleryItem> *)galleryItem;
+
+- (bool)deleteChild:(NSObject <GalleryItem> *)galleryItem;
+
+- (NSString *)saveThumbnailImage;
+
+- (NSString *)saveFullImage;
+
+- (bool)exportToCameraRoll;
+
 @end
