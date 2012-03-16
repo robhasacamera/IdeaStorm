5eb3f04ead3727ecf3e52879cde99741c418a93a
Robert Cole <robhasacamera@gmail.com>
2012-03-16 18:31:27 -0500

    Added code for generating unique IDs.

--- IdeaStorm/Shared/Database.m
+++ IdeaStorm/Shared/Database.m
@@ -74,7 +74,21 @@
 }
 
 + (NSString *)generateUniqueID {
-    return nil;
+    // create a new UUID which you own
+    CFUUIDRef uuid = CFUUIDCreate(kCFAllocatorDefault);
+    
+    // create a new CFStringRef (toll-free bridged to NSString)
+    // that you own
+    NSString *uuidString = (NSString *)CFUUIDCreateString(kCFAllocatorDefault, uuid);
+    
+    // transfer ownership of the string
+    // to the autorelease pool
+    [uuidString autorelease];
+    
+    // release the UUID
+    CFRelease(uuid);
+    
+    return uuidString;
 }
 
 #pragma mark - GalleryItem Management
